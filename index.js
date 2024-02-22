dfabab9abe102d85ab3b68a31afa17aefd24d424
Tijana <tijana.zivkovic@teamcad.rs>
2024-02-22 16:00:23 +0100

    uvecanje slike na click

--- index.js
+++ index.js
@@ -24,3 +24,13 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+function full_view(ele) {
+    let src = ele.parentElement.querySelector(".img-source").getAttribute("src");
+    document.querySelector("#img-viewer").querySelector("img").setAttribute("src", src);
+    document.querySelector("#img-viewer").style.display = "block";
+}
+
+function close_model() {
+    document.querySelector("#img-viewer").style.display = "none";
+}
